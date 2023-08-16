511f2b650dacb58f4822ee1aabc12cd04c183625
Violetta Lutsevich <violet.lutsevich@gmail.com>
2023-08-16 13:19:29 +0300

    перемещение платформы

---
+++ Assets/Scriptes/Paddle/Input/PaddleInputReader.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.InputSystem;
+
+namespace FantasticArcanoid.Paddle
+{
+    public class PaddleInputReader : MonoBehaviour
+    {
+        [SerializeField] private Paddle _paddle;
+        public void OnAxisInput(InputAction.CallbackContext callback)
+        {
+            var direction = callback.ReadValue<float>();
+            _paddle.SetDirection(direction);
+        }
+    }
+}
