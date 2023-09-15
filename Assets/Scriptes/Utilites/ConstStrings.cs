ee26a9a23c2db8c1a3c860f163485d941ec74a19
Violetta Lutsevich <violet.lutsevich@gmail.com>
2023-09-15 22:33:37 +0300

    добавлен тег Brick

--- Assets/Scriptes/Utilites/ConstStrings.cs
+++ Assets/Scriptes/Utilites/ConstStrings.cs
@@ -9,7 +9,8 @@ namespace FantasticArkanoid.Utilites
         public static class Tags
         {
             public const string PADDLE_TAG = "Paddle";
-            public const string BALL_TAG = "Ball"; 
+            public const string BALL_TAG = "Ball";
+            public const string BRICK_TAG = "Brick";
         }
     }
 }
